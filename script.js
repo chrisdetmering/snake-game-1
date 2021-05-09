10ffaa980aca1f41f3efd8735419f36cebeec1aa
VirtualVasquez <mvasq95@hotmail.com>
2021-05-09 18:01:45 -0400

    css, html change. created newApple() function

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const gameDOM = document.getElementById('game-space')
 
-function gridGenerate(){
-    for(let i = 0; i < 400; i++){
-        gameDOM.innerHTML += "<div class='pixel' id=" + (i+1) + "></div>"
-    }
-}
 
 gridGenerate();
+let target = document.getElementById("181")
+target.setAttribute("class", "pixel apple");
+// newApple();
+
 //IF GAME OVER
         //SNAKE TOUCHES ITSELF
         //SNAKE TOUCHES THE OUTSIDE BORDER
@@ -18,3 +17,17 @@ gridGenerate();
     //RESPAWN NEW APPLE AT RANDOM LOCATION
 //CONTROL SNAKE WITH ARROW KEYS ON KEYBOARD
 //SHOW SCORE OF APPLES EATEN
+
+
+function newApple(){
+    target.setAttribute("class", "pixel")
+    let number = Math.floor(Math.random()* 361);
+    target = document.getElementById(number);
+    target.setAttribute("class", "pixel apple")
+}
+
+function gridGenerate(){
+    for(let i = 0; i < 361; i++){
+        gameDOM.innerHTML += "<div class='pixel' id=" + (i+1) + "></div>"
+    }
+}
