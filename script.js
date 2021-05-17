5b91bcb0f4a661f3dc804c51645edc6a4dffe4ca
VirtualVasquez <mvasq95@hotmail.com>
2021-05-16 20:09:34 -0400

    deliverables met

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 let canvas;
 let canvasContext;
 let scoreboard;
-let appleX = 240;
-let appleY = 240;
-let direction = "right";
-let score = 0;
-let snakeWidth = 40;
-let pixelSize = 20;
-
+let appleX;
+let appleY;
+let score;
 let snakeBody;
 let snakeCopy;
 let heartBeat;
+let direction;
+let surprise;
+let fps = 24;
+let pixelSize = 20;
+
+
 
 window.onload = function(){
+    surprise = document.getElementById('game-over')
+    scoreboard = document.getElementById('score');
+    canvas = document.getElementById('game-space')
+    canvasContext = canvas.getContext('2d');
+    newGame();
+}
+
+function newGame(){
+    score = 0
+    scoreboard.innerText = score;
+    canvas.removeAttribute("hidden")
+    surprise.pause();
+    surprise.currentTime = 0;
+    surprise.setAttribute("hidden", true);
     snakeBody = [
         {x: 120, y: 120},
         {x: 100, y: 120},
@@ -20,127 +36,14 @@ window.onload = function(){
         {x: 60, y: 120}
     ]
     snakeCopy = JSON.parse(JSON.stringify(snakeBody));
-    
-    scoreboard = document.getElementById('score');
-    canvas = document.getElementById('game-space')
-    canvasContext = canvas.getContext('2d');
+    appleX = 240;
+    appleY = 240;
+    direction = "right"
 
-    let fps = 24;
     heartBeat = setInterval(function(){
         moveEverything(direction);
         drawEverything();
     }, 1000/fps);
-
-}
-
-function drawEverything(direction){
-    if(direction !== "stop"){
-        canvasContext.fillStyle = "white"
-        canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-    
-        apple = canvasContext;
-        apple.fillStyle = "red"
-        apple.fillRect(appleX, appleY, pixelSize, pixelSize);
-        
-        drawSnake();
-    
-        if (snakeBody[0].x === appleX && snakeBody[0].y === appleY){
-            newApple();
-        }
-    }
-}
-
-document.onkeydown = function(e){
-    if(direction === "stop"){
-        return gameOver();
-    } 
-        switch(e.key){
-            case "ArrowUp":
-                if(direction !== "down")
-                direction = "up";
-                break;
-            case "ArrowDown":
-                if(direction !== "up")
-                direction = "down";
-                break;
-            case "ArrowLeft":
-                if(direction !== "right")
-                direction = "left";
-                break;
-            case "ArrowRight":
-                if(direction !== "left")
-                direction = "right";
-                break;
-        }
-}
-
-function isOuroboros(headX, headY){
-    let match;
-    snakeBody.forEach(pixel =>{
-        if(pixel.x === headX && pixel.y === headY){
-            match = true
-        }
-    })
-    return match
-}
-
-function gameOver(){
-    direction = "stop";
-    console.log("Game Over!");
-    clearInterval(heartBeat)
-}
-
-
-//IF GAME OVER
-    //WHEN GAME OVER
-        //STOP GAME
-        //NOTIFY USER
-
-function drawSnake(){
-    snakeBody.forEach(pixel => {
-        snakePiece = canvasContext;
-        snakePiece.fillStyle = "blue";
-        snakePiece.fillRect(pixel.x, pixel.y, pixelSize, pixelSize)
-    })
-}
-
-function moveBody(){
-    snakeBody = [snakeBody.shift()];
-    snakeCopy.pop()
-    snakeCopy.forEach(element =>{
-        snakeBody.push(element);
-    })
-    snakeCopy = JSON.parse(JSON.stringify(snakeBody));
-}
-
-
-function randomXorY(){
-    return (Math.floor(Math.random() * 24)) * 20;
-}
-
-function newApple(){
-    let x = randomXorY();
-    let y = randomXorY();
-    snakeBody.forEach(pixel =>{
-        if (pixel.x === x && pixel.y === y){
-            x = randomXorY();
-            y = randomXorY();
-        }
-    })
-    apple.clearRect(appleX, appleY, 20, 20)
-    apple.fillRect(x, y, 20, 20);
-    score += 1;
-    scoreboard.innerText = score;
-    appleX = x;
-    appleY = y;
-    snakeWidth += 20;
-    snakeBody.push(
-        {
-            x: snakeBody[snakeBody.length-1].x, 
-            y: snakeBody[snakeBody.length-1].y
-        }
-    )
-    snakeCopy = JSON.parse(JSON.stringify(snakeBody));
 }
 
 function moveEverything(direction){
@@ -195,3 +98,108 @@ function moveEverything(direction){
 
 
 }
+function moveBody(){
+    snakeBody = [snakeBody.shift()];
+    snakeCopy.pop()
+    snakeCopy.forEach(element =>{
+        snakeBody.push(element);
+    })
+    snakeCopy = JSON.parse(JSON.stringify(snakeBody));
+}
+function drawEverything(direction){
+    if(direction !== "stop"){
+        canvasContext.fillStyle = "white"
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    
+        apple = canvasContext;
+        apple.fillStyle = "red"
+        apple.fillRect(appleX, appleY, pixelSize, pixelSize);
+        
+        drawSnake();
+    
+        if (snakeBody[0].x === appleX && snakeBody[0].y === appleY){
+            newApple();
+        }
+    }
+}
+function drawSnake(){
+    snakeBody.forEach(pixel => {
+        snakePiece = canvasContext;
+        snakePiece.fillStyle = "blue";
+        snakePiece.fillRect(pixel.x, pixel.y, pixelSize, pixelSize)
+    })
+}
+
+
+document.onkeydown = function(e){
+    if(direction === "stop"){
+        return newGame();
+    } 
+        switch(e.key){
+            case "ArrowUp":
+                if(direction !== "down")
+                direction = "up";
+                break;
+            case "ArrowDown":
+                if(direction !== "up")
+                direction = "down";
+                break;
+            case "ArrowLeft":
+                if(direction !== "right")
+                direction = "left";
+                break;
+            case "ArrowRight":
+                if(direction !== "left")
+                direction = "right";
+                break;
+        }
+}
+
+function isOuroboros(headX, headY){
+    let match;
+    snakeBody.forEach(pixel =>{
+        if(pixel.x === headX && pixel.y === headY){
+            match = true
+        }
+    })
+    return match
+}
+
+function gameOver(){
+    direction = "stop";
+    canvas.setAttribute("hidden", true)
+    surprise.removeAttribute("hidden")
+    surprise.play();
+    console.log("Game Over!");
+    clearInterval(heartBeat)
+}
+
+function randomXorY(){
+    return (Math.floor(Math.random() * 24)) * 20;
+}
+
+function newApple(){
+    let x = randomXorY();
+    let y = randomXorY();
+    snakeBody.forEach(pixel =>{
+        if (pixel.x === x && pixel.y === y){
+            x = randomXorY();
+            y = randomXorY();
+        }
+    })
+    apple.clearRect(appleX, appleY, pixelSize, pixelSize)
+    apple.fillRect(x, y, pixelSize, pixelSize);
+    score += 1;
+    scoreboard.innerText = score;
+    appleX = x;
+    appleY = y;
+    snakeBody.push(
+        {
+            x: snakeBody[snakeBody.length-1].x, 
+            y: snakeBody[snakeBody.length-1].y
+        }
+    )
+    snakeCopy = JSON.parse(JSON.stringify(snakeBody));
+}
+
+
